0dbec1537432fd71cbfeec925436bc08a517e692
Parth Mishra <114911403+parthmishra0601@users.noreply.github.com>
2024-10-13 22:29:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 import Courses from './Components/Courses'; // Ensure this path is correct
 import SearchResults from './Components/SearchResults'; // Import the SearchResults component
-
-import Quiz from './Components/quiz';
+import Quiz from './Components/quiz'; // Import the Quiz component
+import Chatbot from './Components/Chatbot'; // Import the Chatbot component
 
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Navbar /> {/* Navbar should be above the Routes */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Protected routes below */}
+        <Route path="/" element={<Home />} /> 
         <Route path="/courses" element={<Courses />} />
-        <Route path="/search/:searchTerm" element={<SearchResults />} /> {/* Route for search results */}
-        <Route path="/quiz" element={<Quiz/>} /> {/* Route for quiz */}
-        <Route path="/quiz/:searchQuery" element={<Quiz/>} /> {/* Route for quiz with search query */}
-        {/* Define other routes here */}
+        <Route path="/search/:searchTerm" element={<SearchResults />} /> 
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quiz/:searchQuery" element={<Quiz />} /> 
+        <Route path="/chatbot" element={<Chatbot />} /> 
+        {/* End of protected routes */}
+
+     
+
       </Routes>
     </Router>
   );
